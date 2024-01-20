47bd5cdf684551a4e96b57aaf66f403a0b244493
Seth Ketsi <christambassador12@gmail.com>
2024-01-20 21:58:10 +0000

    updated

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -17,7 +17,7 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         """ Permiter of square"""
         return (self.width * 2) + (self.height * 2)
 
@@ -31,4 +31,4 @@ if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
