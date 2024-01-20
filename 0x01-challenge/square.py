a71b06b683c7ea07582ccc120828a7c3ca99fc45
Seth Ketsi <christambassador12@gmail.com>
2024-01-20 21:46:06 +0000

    Fix my code - Square and User Fixed

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module for Square class"""""
+
+
+class square():
+    """Square class"""
+
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def PermiterOfMySquare(self):
+        """ Permiter of square"""
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """ Print the square"""
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    """ Create a square"""
+    s = square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
